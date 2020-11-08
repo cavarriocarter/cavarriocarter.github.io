814f5d06d5f4f04150f9acddfe141e0be657142a
Cavarrio Carter <cavarriocarter8614@outlook.com>
2020-11-07 23:57:21 -0600

    Initial commit

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,7 @@
-window.g2 = require('@antv/g2');
-window.react = require('react');
-window.reactDom = require('react-dom');
+/**
+ * Implement Gatsby's Browser APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/browser-apis/
+ */
+
+// You can delete this file if you're not using it
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,78 +1,24 @@
 module.exports = {
-  pathPrefix: "/cavarriocarter.github.io",
-  plugins: [
-    {
-      resolve: '@antv/gatsby-theme-antv',
-      options: {}
-    }
-  ],
   siteMetadata: {
-    title: 'AntV',
-    description: 'Gatsby starter of antv theme',
-    siteUrl: 'https://gatsby-starter-theme-antv.antv.vision',
-    githubUrl: 'https://github.com/antvis/antvis.github.io',
-    navs: [
-      {
-        slug: 'docs/specification/getting-started',
-        title: {
-          zh: '设计语言',
-          en: 'Specification'
-        }
+    title: "Gatsby Starter - Forty V2",
+    author: "Hunter Chang",
+    description: "A Gatsby.js V2 Starter based on Forty by HTML5 UP"
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
       },
-      {
-        slug: 'examples',
-        title: {
-          zh: '图表演示',
-          en: 'Examples'
-        }
-      },
-      {
-        slug: 'independent',
-        title: {
-          zh: '独立',
-          en: 'MyApp'
-        }
-        // target: '_blank',
-      }
-    ],
-    docs: [
-      {
-        slug: 'specification/category',
-        title: {
-          zh: '分类一',
-          en: 'category1'
-        },
-        order: 4
-      },
-      {
-        slug: 'specification/category/three',
-        title: {
-          zh: '第三层',
-          en: 'three level'
-        },
-        order: 2
-      }
-    ],
-    examples: [
-      {
-        slug: 'category',
-        icon: 'pie',
-        title: {
-          zh: '饼图分类',
-          en: 'Category'
-        }
-      }
-    ],
-    showSearch: false, // 是否展示搜索框
-    showChinaMirror: false, // 是否展示国内镜像链接
-    showAntVProductsCard: false, // 是否展示 AntV 系列产品的卡片链接
-    showLanguageSwitcher: true, // 用于定义是否展示语言切换
-    showGithubCorner: true, // 是否展示角落的 GitHub 图标
-    playground: {
-      container: '<div id="container" />',
-      playgroundDidMount: 'console.log("playgroundDidMount");',
-      playgroundWillUnmount: 'console.log("playgroundWillUnmount");'
     },
-    redirects: []
-  }
-};
+    'gatsby-plugin-sass',
+    'gatsby-plugin-offline'
+  ],
+}
